2e58fddc2e798f1feedc01574b00ede14788d619
user <user@userui-MacBookAir.local>
2022-08-16 01:00:53 +0900

    2022-08-16 login(jwt token)추가 (영욱)

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1y' },
+    }),
+  ],
+  providers: [AuthService],
+})
+export class AuthModule {}
