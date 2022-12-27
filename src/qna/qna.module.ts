48318913311fd3cb353bf14bdcc8d7de67821f29
YeongWook <ehdrnr16whduddnr@naver.com>
2022-12-28 00:04:43 +0900

    2022-12-28 answer생성 예외처리(영욱)

--- src/qna/qna.module.ts
+++ src/qna/qna.module.ts
@@ -4,9 +4,10 @@ import { QnaService } from './service/qna.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Qna } from '../entity/qna.entity';
 import { QnaRepository } from './qna.repository';
+import { ProductModule } from '../product/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Qna])],
+  imports: [TypeOrmModule.forFeature([Qna]), ProductModule],
   controllers: [QnaController],
   providers: [QnaService, QnaRepository],
   exports: [QnaService, QnaRepository],
